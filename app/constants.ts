11f797ea4a8aa763a60163c48ed2e2d99b4d0c35
deepbluewarn <n5lp97@gmail.com>
2024-02-13 15:25:37 +0900

    feat: 닉네임 등록 기능 추가, redis 설정 추가

--- app/constants.ts
+++ app/constants.ts
@@ -1,1 +1,7 @@
 export const BOARD_SIZE = 15;
+export const REDIS_SETTING = {
+    host: 'localhost',
+    port: 5444,
+    password: process.env.NEXT_PUBLIC_REDIS_PASSWORD,
+    db: 0
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,21 +3,36 @@
 import Board from "@/components/board/Board";
 import styles from './page.module.css';
 import { useSocket } from "@/components/socket-provider";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
   const { socket } = useSocket();
+  const [nickname, setNickname] = useState(''); // TODO: 닉네임 입력 필드 구현
+
+  const requestRegister = async () => {
+    await fetch(`https://${process.env.NEXT_PUBLIC_API_DOMAIN}/register`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'ngrok-skip-browser-warning': '4444',
+      },
+      credentials: 'include',
+      body: JSON.stringify({ nickname }),
+    });
+  }
 
   useEffect(() => {
-    if(typeof socket === 'undefined') return;
-
-    socket?.emit('client', '안녕하세요');
+    if (typeof socket === 'undefined') return;
   }, [socket])
 
   return (
     <main className={styles.main}>
       <div>Header</div>
-      <Board />
+      <div>
+        <input type="text" value={nickname} onChange={(e) => {setNickname(e.target.value)}}/>
+        <button onClick={() => { requestRegister() }}>가입하기</button>
+      </div>
+      {/* <Board /> */}
       <div>Footer</div>
     </main>
   );
