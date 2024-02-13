6c72dcb349c3662653e6e181f94439ec10d67d71
deepbluewarn <n5lp97@gmail.com>
2024-02-13 15:24:09 +0900

    feat: 소켓 통신 기본 기능 구현

--- components/board/Board.tsx
+++ components/board/Board.tsx
@@ -1,9 +1,27 @@
 import { BOARD_SIZE } from '@/app/constants';
 import styles from './board.module.css';
+import { useSocket } from '../socket-provider';
+import { useEffect } from 'react';
 
 export default function Board() {
+    const { socket } = useSocket();
+
+    const onCellClick = (cell_num: number) => {
+        if(!socket) return;
+
+        socket.emit('place', cell_num);
+    }
+
+    useEffect(() => {
+        if(!socket) return;
+        
+        socket?.on('placed', (data) => {
+            console.log('[착수 응답] data: ', data);
+        });
+    }, [socket]);
+
     const cells = Array(BOARD_SIZE * BOARD_SIZE).fill(null).map((_, idx) => {
-        return <div key={idx} className={styles.cell} />;
+        return <div key={idx + 1} className={styles.cell} onClick={() => {onCellClick(idx + 1)}}>{idx + 1}</div>;
     });
     
     return (
