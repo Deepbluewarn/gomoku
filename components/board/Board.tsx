e173c4866d60887666fd7d28e8eadf601c949aa0
deepbluewarn <n5lp97@gmail.com>
2024-02-27 12:55:23 +0900

    fix: 보드 생성 코드 수정

--- components/board/Board.tsx
+++ components/board/Board.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import { BOARD_SIZE } from '@/app/constants';
 import styles from './board.module.css';
 import { useSocket } from '../socket-provider';
@@ -5,21 +7,25 @@ import { Fragment, useCallback, useEffect, useState } from 'react';
 
 type StoneType = 'none' | 'black' | 'white';
 
-export default function Board() {
+export default function Board(props: {room_id: string}) {
     const { socket } = useSocket();
     const [board, setBoard] = useState<StoneType[][]>(Array(BOARD_SIZE).fill(Array(BOARD_SIZE).fill('none')));
 
     const onCellClick = (cell_num: number) => {
         if (!socket) return;
 
-        socket.emit('place', cell_num);
+        socket.emit('place', {
+            room_id: props.room_id,
+            cell_num
+        });
     }
 
     useEffect(() => {
         if (!socket) return;
 
-        socket?.on('placed', (data) => {
-            place(data, 'white');
+        socket.on('placed', (data) => {
+            console.log('placed: ', data);
+            place(data.cell_num, 'white');
         });
     }, [socket]);
 
@@ -28,8 +34,8 @@ export default function Board() {
         let j = cell_num % BOARD_SIZE - 1;
 
         if(cell_num % BOARD_SIZE === 0) {
-            i--;
-            j = BOARD_SIZE;
+            i = --i;
+            j = BOARD_SIZE - 1;
         }
 
         setBoard((prev) => {
@@ -59,18 +65,19 @@ export default function Board() {
     const cellStyle = styles['cell'];
     const stoneBlackStyle = styles['cell-black-stone'];
     const stoneWhiteStyle = styles['cell-white-stone'];
-
+    const distance = 100 / (BOARD_SIZE + 1);
     const stones: JSX.Element[] = [];
 
     for (let i = 0; i < board.length; i++) {
         for (let j = 0; j < board[i].length; j++) {
             const cell = board[i][j];
             const stoneStyle = cell === 'black' ? stoneBlackStyle : cell === 'white' ? stoneWhiteStyle : cellStyle;
+            
             stones.push(
                 <circle
                     key={`${cell}-${i}-${j}`}
-                    cx={`${(j + 1) * 100 / (BOARD_SIZE + 1)}%`}
-                    cy={`${(i + 1) * 100 / (BOARD_SIZE + 1)}%`}
+                    cx={`${((j + 1) * distance)}%`}
+                    cy={`${((i + 1) * distance)}%`}
                     r="2.4%"
                     className={`${stoneStyle}`}
                     onClick={() => onCellClick(i * BOARD_SIZE + j + 1)}
